7712e7964e3852fc492c05f905857d4d941e0285
ManassawinDm <manassawin.dm@gmail.com>
2024-04-19 14:37:24 +0700

    reset

--- controllers/main.js
+++
@@ -1,9 +0,0 @@
-// Display the Welcome Page
-exports.getWelcomePage = (req, res, next) => {
-  res.render("welcome", { req, pageTitle: "Welcome", path: "/welcome" });
-};
-
-// Display the Main Page
-exports.getMainPage = (req, res, next) => {
-  res.render("main", { req, pageTitle: "", path: "/home" });
-};
