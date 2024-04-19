3427d33868b83450bd0bb3d9c1ef604c7c716f96
ManassawinDm <manassawin.dm@gmail.com>
2024-04-19 09:14:51 +0700

    kp

--- controllers/form.js
+++ controllers/form.js
@@ -1,7 +1,28 @@
+const db = require("../db.js");
+
 exports.getform = (req, res, next) => {
     res.render("form", { req, pageTitle: "form"});
   };
 
 exports.getformCdp = (req, res, next) => {
-    res.render("form-cdp", { req, pageTitle: "form"});
+    const cityID = req.session.userID;
+  const q = "SELECT * FROM solution JOIN smart ON solution.smartKey = smart.smartKey JOIN kpi ON kpi.solutionID = solution.solutionID JOIN city_home ON city_home.cityID = solution.cityID WHERE solution.cityID = ? GROUP BY solution.solutionName";
+  try{
+    db.query(q, [cityID], (err, data) => {
+        console.log(data)
+      if (err) return res.status(500).json(err);
+      res.render("form-cdp", {
+        formdata: data
+      });
+    });
+
+
+
+  }catch(err){
+    console.log(err);
+    res.status(500).json(err)
+  }
   };
+
+
+  
