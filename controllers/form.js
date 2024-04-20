14de04f1d63171828e04c242f24872571d58a734
ManassawinDm <manassawin.dm@gmail.com>
2024-04-20 22:40:43 +0700

    navbar update

--- controllers/form.js
+++ controllers/form.js
@@ -9,7 +9,7 @@ exports.getformCdp = (req, res, next) => {
   const cityID = req.session.userID;
   const q = "SELECT * FROM solution JOIN smart ON solution.smartKey = smart.smartKey JOIN kpi ON kpi.solutionID = solution.solutionID JOIN city_home ON city_home.cityID = solution.cityID WHERE solution.cityID = ? AND solution.solutionID = ? ";
   try{
-    db.query(q, [cityID,solutionid], (err, data) => {
+    db.query(q, [cityID,solutionid], (err, data) => { 
       console.log(data)
       if (err) return res.status(500).json(err);
       res.render("form-cdp", {
