e19877f464a6e229544ab0681767d9a3bbbe6d7d
ManassawinDm <manassawin.dm@gmail.com>
2024-04-22 17:24:06 +0700

    update follow form

--- controllers/form.js
+++ controllers/form.js
@@ -16,10 +16,9 @@ exports.getformCdp = (req, res, next) => {
       
       db.query(q2, [solutionid], (err, dataOld) => {
         if (err) return res.status(500).json(err);
-        
         res.render("form-cdp", {
           formdata: data,
-          dataOld: dataOld || [],
+          dataOld: dataOld|| [],
           csrfToken: req.csrfToken()
         });
       });
@@ -36,35 +35,42 @@ exports.getformCdp = (req, res, next) => {
 
 exports.postFormCdp = (req, res, next) => {
   try {
-    console.log(req.body)
-    const postData = req.body
-    const solutionParam = req.params
-    const qInsert = "INSERT INTO anssolution (solutionID, timestamp, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9) VALUES (?,?,?,?,?,?,?,?,?,?,?)"
-    const qFechdata = "SELECT * FROM anssolution WHERE solutionID = ?"
-    const qUpdate = "UPDATE anssolution SET timestamp = ?, Q1=?, Q2=?, Q3=?, Q4=?, Q5=?, Q6=?, Q7=?, Q8=?, Q9=? WHERE solutionID=?;"
-    db.query(qFechdata,[solutionParam.solutionID],(err,fechData)=>{
-      if(err) return res.status(500).json("FechdataError",err)
-      if (fechData && fechData.length > 0) {
-        db.query(qUpdate,[postData.currentDateTime,postData.status,postData.progress,postData.fileUpload,postData.operation,
-          postData.problem_type,postData.result,postData.problem,postData.solution,postData.note,solutionParam.solutionID],(err,updataData)=>{
-            if(err) return res.status(500).json("updataError",err)
-            return res.redirect(`/formcdp/${req.params.solutionID}?success=true`);
-          })
-      } else {
-        db.query(qInsert,[solutionParam.solutionID,postData.currentDateTime,postData.status,postData.progress,postData.fileUpload,postData.operation,
-          postData.problem_type,postData.result,postData.problem,postData.solution,postData.note],(err,insertData)=>{
-            if(err) return res.status(500).json("insertdataError",err)
+    console.log(req.body);
+    const postData = req.body;
+    const solutionParam = req.params;
+    const qInsert = "INSERT INTO anssolution (solutionID, timestamp, Q1, Q2, Q3, Q4, Q5, Q6, Q7, Q8, Q9) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+    const qUpdateStatus = "UPDATE solution SET status = 1 WHERE solutionID = ?";
+    const qFechdata = "SELECT * FROM anssolution WHERE solutionID = ?";
+    const qUpdate = "UPDATE anssolution SET timestamp = ?, Q1=?, Q2=?, Q3=?, Q4=?, Q5=?, Q6=?, Q7=?, Q8=?, Q9=? WHERE solutionID=?;";
 
+    db.query(qFechdata, [solutionParam.solutionID], (err, fechData) => {
+      if (err) return res.status(500).json({ error: "FechdataError", message: err });
+      if (fechData && fechData.length > 0) {
+        db.query(qUpdate, [postData.currentDateTime, postData.Q1, postData.Q2, postData.Q3, postData.Q4, postData.Q5, postData.Q6, postData.Q7, postData.Q8, postData.Q9, solutionParam.solutionID], (err, updateData) => {
+          if (err) return res.status(500).json({ error: "updateError", message: err });
+          db.query(qUpdateStatus, [solutionParam.solutionID], (err, updateStatusData) => {
+            if (err) return res.status(500).json({ error: "updateStatusError", message: err });
             return res.redirect(`/formcdp/${req.params.solutionID}?success=true`);
-          })
+          });
+        });
+      } else {
+        db.query(qInsert, [solutionParam.solutionID, postData.currentDateTime, postData.status, postData.progress, postData.fileUpload, postData.operation, postData.problem_type, postData.result, postData.problem, postData.solution, postData.note], (err, insertData) => {
+          if (err) return res.status(500).json({ error: "insertDataError", message: err });
+          db.query(qUpdateStatus, [solutionParam.solutionID], (err, updateStatusData) => {
+            if (err) return res.status(500).json({ error: "updateStatusError", message: err });
+            return res.redirect(`/formcdp/${req.params.solutionID}?success=true`);
+          });
+        });
       }
-    })
-    
+    });
+
   } catch (err) {
     console.log(err);
-    res.status(500).json(err)
+    res.status(500).json({ error: "Internal Server Error", message: err.message });
   }
 };
+
+
 
 
 
