af30ac1ce485823c07e4ce0c0ed58758a0a6f4c8
Keng_Noppadon <kang4930.4930@gmail.com>
2024-04-23 08:53:11 +0700

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,8 +1,7 @@
 // **************
 // **  Models  **
 // **************
-const City = require("../models/city");
-const CityData = require("../models/cityData");
+
 
 exports.getAdPage = (req, res, next) => {
   res.render("admin/ad-main", {
@@ -12,56 +11,11 @@ exports.getAdPage = (req, res, next) => {
 };
 
 exports.getAdCityP = (req, res, next) => {
-  City.find({}).then((cities) => {
-    // console.log(cities)
-    res.render("admin/ad-city/ad-city", {
-      pageTitle: "City",
-      path: "/",
-      cities: cities,
-    });
-  });
+
 };
 
 exports.getAdCityDataP = (req, res, next) => {
-  const cityID = req.params.cityID;
-  console.log(cityID);
 
-  // Define the aggregation pipeline
-  const pipeline = [
-    {
-      $lookup: {
-        from: "citydata",
-        localField: "cityID", 
-        foreignField: "cityID", 
-        as: "citydata", 
-      },
-    },
-    {
-      $unwind: "$orders",
-    },
-    // Add more stages for filtering or data manipulation as needed
-  ];
-
-  CityData.findOne({cityID: cityID}).then(citydata =>{
-    console.log(citydata)
-  }
-  )
-
-  City.aggregate(pipeline)
-  .then(cityDataA => {
-    // Process the joined data (users with their respective orders)
-    console.log(cityDataA)
-
-
-     res.render('admin/ad-city/ad-cityData', {
-          pageTitle: 'City',
-          path: '/',
-        //   city: city,
-      });
-  })
-  .catch(error => {
-    console.error(error);
-  });
 
 //   City.findOne({ cityID: cityID });
 
