d608fc0617cb3d2e51d7539ae37021f80408ef9e
Keng_Noppadon <kang4930.4930@gmail.com>
2024-04-24 16:03:19 +0700

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,7 @@
 // **************
 // **  Models  **
 // **************
-
+const db = require("../db.js");
 
 exports.getAdPage = (req, res, next) => {
   res.render("admin/ad-main", {
@@ -10,22 +10,56 @@ exports.getAdPage = (req, res, next) => {
   });
 };
 
+exports.getAddUserPage=(req,res,next)=>{
+  res.render("admin/ad-city/ad-addUser",{
+    pageTitle: "add",
+    path: "/",
+  });
+}
+//get city ad-city
 exports.getAdCityP = (req, res, next) => {
-
+  const q = "SELECT `cityID`, `province`, `date`, `developer`, `executive`, `government_investment`, `private_investment`,`LAT`, `LNG` FROM `citydata` WHERE 1";
+  try {
+    db.query(q, (err, data) => {
+      if (err) return res.status(500).json(err);
+      console.log(data)
+      res.render("admin/ad-city/ad-city", {
+        req,
+        pageTitle: "Dashboard",
+        path: "/city",
+        cityData: data,
+      });
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 };
 
 exports.getAdCityDataP = (req, res, next) => {
-
-
-//   City.findOne({ cityID: cityID });
-
-  // .then(city  => {
-  //     console.log(city)
-
-  //     res.render('admin/ad-city/ad-cityData', {
-  //         pageTitle: 'City',
-  //         path: '/',
-  //         city: city,
-  //     });
-  // })
+  console.log(req.params)
+  const q = "SELECT * FROM citydata JOIN city_home ON citydata.cityID = city_home.cityID WHERE citydata.cityID = ?;";
+  const q2 ="SELECT * FROM `solution` JOIN smart ON solution.smartKey = smart.smartKey JOIN kpi on kpi.solutionID=solution.solutionID WHERE solution.cityID=? GROUP BY solution.solutionName"
+  try {
+    db.query(q,[req.params.cityID], (err, data) => {
+      if (err) return res.status(500).json(err);
+      console.log("Data is:",data)
+      db.query(q2,[req.params.cityID],(errer,solution)=>{
+        if(err)return res.status(500).json(errer)
+        // console.log("solution is:",solution)
+      res.render("admin/ad-city/ad-citydata", {
+        req,
+        pageTitle: "Dashboard",
+        path: "/city",
+        cityData: data[0],
+        solution:solution,
+      });
+      })
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 };
+
+// SELECT * FROM citydata JOIN city_home ON citydata.cityID = city_home.cityID JOIN solution ON citydata.cityID=solution.cityID WHERE citydata.cityID = 6201;
