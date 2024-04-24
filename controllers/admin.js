5eecb4e2c2cefd44900bf317f30a4dd22e7df300
Keng_Noppadon <kang4930.4930@gmail.com>
2024-04-24 17:03:21 +0700

    Update admin.js

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,7 +2,7 @@
 // **  Models  **
 // **************
 const db = require("../db.js");
-
+const bcrypt = require('bcrypt');
 exports.getAdPage = (req, res, next) => {
   res.render("admin/ad-main", {
     pageTitle: "Main",
@@ -10,19 +10,20 @@ exports.getAdPage = (req, res, next) => {
   });
 };
 
-exports.getAddUserPage=(req,res,next)=>{
-  res.render("admin/ad-city/ad-addUser",{
+exports.getAddUserPage = (req, res, next) => {
+  res.render("admin/ad-city/ad-addUser", {
     pageTitle: "add",
     path: "/",
   });
-}
+};
 //get city ad-city
 exports.getAdCityP = (req, res, next) => {
-  const q = "SELECT `cityID`, `province`, `date`, `developer`, `executive`, `government_investment`, `private_investment`,`LAT`, `LNG` FROM `citydata` WHERE 1";
+  const q =
+    "SELECT `cityID`, `province`, `date`, `developer`, `executive`, `government_investment`, `private_investment`,`LAT`, `LNG` FROM `citydata` WHERE 1";
   try {
     db.query(q, (err, data) => {
       if (err) return res.status(500).json(err);
-      console.log(data)
+      console.log(data);
       res.render("admin/ad-city/ad-city", {
         req,
         pageTitle: "Dashboard",
@@ -37,24 +38,26 @@ exports.getAdCityP = (req, res, next) => {
 };
 
 exports.getAdCityDataP = (req, res, next) => {
-  console.log(req.params)
-  const q = "SELECT * FROM citydata JOIN city_home ON citydata.cityID = city_home.cityID WHERE citydata.cityID = ?;";
-  const q2 ="SELECT * FROM `solution` JOIN smart ON solution.smartKey = smart.smartKey JOIN kpi on kpi.solutionID=solution.solutionID WHERE solution.cityID=? GROUP BY solution.solutionName"
+  console.log(req.params);
+  const q =
+    "SELECT * FROM citydata JOIN city_home ON citydata.cityID = city_home.cityID WHERE citydata.cityID = ?;";
+  const q2 =
+    "SELECT * FROM `solution` JOIN smart ON solution.smartKey = smart.smartKey JOIN kpi on kpi.solutionID=solution.solutionID WHERE solution.cityID=? GROUP BY solution.solutionName";
   try {
-    db.query(q,[req.params.cityID], (err, data) => {
+    db.query(q, [req.params.cityID], (err, data) => {
       if (err) return res.status(500).json(err);
-      console.log("Data is:",data)
-      db.query(q2,[req.params.cityID],(errer,solution)=>{
-        if(err)return res.status(500).json(errer)
+      console.log("Data is:", data);
+      db.query(q2, [req.params.cityID], (errer, solution) => {
+        if (err) return res.status(500).json(errer);
         // console.log("solution is:",solution)
-      res.render("admin/ad-city/ad-citydata", {
-        req,
-        pageTitle: "Dashboard",
-        path: "/city",
-        cityData: data[0],
-        solution:solution,
+        res.render("admin/ad-city/ad-citydata", {
+          req,
+          pageTitle: "Dashboard",
+          path: "/city",
+          cityData: data[0],
+          solution: solution,
+        });
       });
-      })
     });
   } catch (err) {
     console.log(err);
@@ -62,4 +65,26 @@ exports.getAdCityDataP = (req, res, next) => {
   }
 };
 
-// SELECT * FROM citydata JOIN city_home ON citydata.cityID = city_home.cityID JOIN solution ON citydata.cityID=solution.cityID WHERE citydata.cityID = 6201;
+exports.getAddCity = (req, res, next) => {
+  res.render("admin/ad-city/ad-addCity", {
+    pageTitle: "add",
+    path: "/",
+  });
+};
+exports.postAddCity = (req, res, next) => {
+  const { cityID, province, cityName, date, developer, executive, government_investment, private_investment, username, password, password2, LAT, LNG } = req.body;
+
+  // ใช้ bcrypt เพื่อเข้ารหัส password
+  bcrypt.hash(password, 10, (err, hashedPassword) => {
+      if (err) {
+          console.error("Error hashing password:", err);
+          return res.status(500).send("Internal Server Error");
+      }
+
+      // ทำสิ่งที่ต้องการด้วย hashedPassword ที่เข้ารหัสแล้ว
+      console.log("Hashed Password:", hashedPassword);
+
+      // ตอบกลับหลังจากทำการเข้ารหัส password แล้ว
+      res.status(200).send("Successfully");
+  });
+};
