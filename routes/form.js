9bf3ecb31665a30f0005e44b3ca8039a9994aeb8
Keng_Noppadon <kang4930.4930@gmail.com>
2024-05-20 10:18:10 +0700

    Re Enegineer code

--- routes/form.js
+++ routes/form.js
@@ -5,30 +5,6 @@ const formController = require('../controllers/form.js');
 const isAuth = require('../middlewares/is-auth.js');
 const router = express.Router();
 
-//open form
-router.get('/form',isAuth,formController.getform);
 
-//Check before installing into database 
-router.post('/formcheck/:solutionID',isAuth,formController.postFormcheck);
-router.post('/comfirmformcheck/:solutionID',isAuth,formController.comfirmFormcheck);
-
-router.post('/formcheck_round2/:solutionID',isAuth,formController.postFormcheck2);
-router.post('/comfirmformcheck_round2/:solutionID',isAuth,formController.comfirmFormcheck2);
-
-//select form
-router.get('/formsmart/:solutionID',isAuth,formController.getformSmart);
-
-//select form round 2
-router.get('/formsmart_round2/:solutionID',isAuth,formController.getformSmartRound2);
-
-//send data form to backebd 
-router.post('/sendformsmart/:solutionID',isAuth,formController.postFormSmart);
-
-//formcdp
-router.get('/formcdp1/:solutionID',isAuth,formController.getformCdp1);
-
-
-//saveform
 router.post('/saveForm/:solutionID',isAuth,formController.saveAnsObj);
-router.post('/saveformsmart_round2/:solutionID',isAuth,formController.saveFormRound2);
 module.exports = router;
