155499bb7bb01dd4794ddec8fe55931125f57d8d
ManassawinDm <manassawin.dm@gmail.com>
2024-07-26 10:57:59 +0700

    mysql2

--- controllers/main.js
+++ controllers/main.js
@@ -49,7 +49,7 @@ GROUP BY cityID;
                         years: duration.years(),
                         months: duration.months(),
                         days: duration.days(),
-                        averageProgress: cityProgress ? cityProgress.AvgProgress.toFixed(0) : "N/A", // Format average progress to 0 decimal places
+                        averageProgress: cityProgress ? cityProgress.AvgProgress : "N/A", // Format average progress to 0 decimal places
                         completionPercentage: cityCompletionInfo.PercentageCompleted
                     };
                 });
